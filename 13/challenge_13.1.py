7282874215ea0cc951c0747533fa1d7baa5163f1
hrshenk <hrshenk@hotmail.com>
2017-02-02 15:11:51 +0000

    Implemented A* solution

--- 13/challenge_13.1.py
+++ 13/challenge_13.1.py
@@ -1,5 +1,5 @@
 from itertools import *
-from collections import deque
+from heapq import *
 
 def test_parity(x, y, favorite):
     num = (x*x + 3*x + 2*x*y + y + y*y) + favorite
@@ -14,18 +14,34 @@ def bit_count(num):
             count += 1
         num = num >> 1
     return count
-    
+
+
+def taxicab_distance(point1, point2):
+    d = 0
+    for x, y in zip(point1, point2):
+        d += abs(x - y)
+    return d
+        
+        
 #define our input
-#favorite = 1350
-favorite = 10
-target = (7, 4)
-current = deque()
-frontier = deque()
+favorite = 1350
+#favorite = 10
+#target = (7, 4)
+target = (31, 39)
+start = (1, 1)
+heap = []  #this will be our priority queue
 seen = {}
-level = 0
-current.appendleft( (1, 1) )
-while current:
-    x, y = current.pop()
+cost = 0
+#our heap will be prioritize by estimated cost to arrive at target
+#and will contain tuples of the form 
+#(estimated cost, current cost to arrive at node, node coordinates)
+heappush(heap, ( taxicab_distance(start, target), 0, start))
+while heap:
+    item = heappop(heap)
+    cost = item[1] + 1 #this is the cost of arriving at neighbors via the current path
+    x, y = item[2]
+    seen[(x, y)] = True
+    #edges will contain nodes adjacent to the current node
     edges = []
     for i in [-1, 1]:
         if x + i >= 0:
@@ -34,18 +50,13 @@ while current:
             edges.append( (x, y+i) )
     for x, y in edges:
         if (x, y) == target:
-            print level + 1
+            print cost
             quit()
         if not test_parity(x, y, favorite):
             if not seen.has_key( (x, y) ):
-                frontier.appendleft( (x, y) )
+                heappush( heap, (cost + taxicab_distance((x,y), target), cost, (x, y)) )
     seen[(x, y)] = True
-    if not current:
-        level += 1
-        print level
-        print len(frontier)
-        current = frontier
-        frontier = deque()
+
         
 
 
